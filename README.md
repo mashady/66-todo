87c8f9f49d6c276d522bd22c72a61bb4955cec25
mashady <muhammedmashady@gmail.com>
2022-03-29 12:33:10 +0200

    yummy

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # 66-todo
-
+## من انتاجات سفاح 
 ![alt text](https://github.com/mashady/66-todo/blob/main/cver.jpg)
 
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./app.scss"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>this projct will be created by SAFAH soon</p>
+        <p className="text-center">this projct will be created by SAFAH soon</p>
       </header>
     </div>
   );
